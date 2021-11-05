081361b33489d81705f397983e081dfbf7aace52
hpzzz <harasimkar@gmail.com>
2021-11-05 01:26:24 +0100

    add dummy json response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "status: available\n")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	//Declare an envelope map containing the data for the response. Notice the way weve constructed this means the environment and version data will now be nestes under a system_info key in the json reponse
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
+
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+// Nest json response in another field?
+type envelope map[string]interface{}
+
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	return id, err
+
+}
+
+// Change the data parameter to have the type envelope instead of interface
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	// encode data to JSON, returning the error if there was one
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	// Append a new line to make it easier to view in terminal applciations
+	js = append(js, '\n')
+
+	// Loop through the header map and add each header to the http.responsewriter header map
+	// note that it's ok if the provided header map is nil/ Go doesnt throw an error if your try to range over a nil map
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 
 }
